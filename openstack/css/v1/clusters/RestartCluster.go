c0a63a4502bfd8f68dee29a99a6f6349b11edf2e
Vineet Pruthi <Vineet.Pruthi@hotmail.com>
2025-04-07 09:17:21 +0200

    Test

--- openstack/css/v1/clusters/RestartCluster.go
+++ openstack/css/v1/clusters/RestartCluster.go
@@ -9,3 +9,10 @@ func RestartCluster(client *golangsdk.ServiceClient, clusterID string) error {
 	})
 	return err
 }
+
+func RestartCluster2(client *golangsdk.ServiceClient, clusterID string) error {
+	_, err := client.Post(client.ServiceURL("clusters", clusterID, "restart"), nil, nil, &golangsdk.RequestOpts{
+		OkCodes: []int{200},
+	})
+	return err
+}
